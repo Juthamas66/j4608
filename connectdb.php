735ff1496c75b829687d182653b672486051eb82
Juthamas66 <65010914608@msu.ac.th>
2025-02-25 14:09:04 +0700

    a

--- connectdb.php
+++ connectdb.php
@@ -1,8 +1,8 @@
 <?php 
 $host = "localhost";
 $user = "root";
-//$pwd = "87654321";
-$pwd = "";
+$pwd = "87654321";
+//$pwd = "";
 $db = "shop4608";
 
 $conn = mysqli_connect ($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
